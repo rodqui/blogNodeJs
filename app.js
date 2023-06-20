6b8dfd0fac318710e295bd6205681cc6823ef395
rodqui <110335551+rodqui@users.noreply.github.com>
2023-06-20 23:18:38 +0200

    Last Update

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const ejs = require("ejs");
 const lodash = require("lodash");
 const port = 3000;
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "";
+const aboutContent = "Mi diario es un espacio digital diseñado para que los usuarios puedan escribir, organizar y compartir sus pensamientos, experiencias y reflexiones de manera privada. Este tipo de sitio web ofrece un entorno seguro y personalizado donde los usuarios pueden registrar y mantener un registro íntimo de sus vivencias, metas, emociones y cualquier otro aspecto relevante de sus vidas.";
+const contactContent = "Anonimo :)";
 
 const app = express();
 
 //to storage the posts
 const posts = [];
+
+//day
+const day = getDay();
 
 app.set('view engine', 'ejs');
 
@@ -32,14 +35,18 @@ app.get("/contact", (req, res)=>{
 });
 
 app.get("/compose", (req, res)=>{
-
-  res.render("compose");
+  
+  res.render("compose", {date: day});
 });
 
 app.post("/compose", (req, res)=>{
+
+  
   const post = {
     title: req.body.postTitle,
-    body: req.body.postText
+    body: req.body.postText,
+    date: req.body.date,
+    autor: req.body.postAutor
   }
 
   posts.push(post);
@@ -56,7 +63,7 @@ app.get("/posts/:postName", (req, res)=>{
       console.log("Match Found!");
       console.log(post.title);
       console.log(post.body);
-      res.render("post",{postTitle: post.title, postBody: post.body});
+      res.render("post",{postTitle: post.title, postBody: post.body, postAutor: post.autor, postDate: post.date});
       
     }
   });
@@ -68,3 +75,18 @@ app.get("/posts/:postName", (req, res)=>{
 app.listen(port, function() {
   console.log("Server started on port " + port);
 });
+
+
+
+function getDay(){
+    
+  const today = new Date();
+  const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+  };
+
+  
+  return today.toLocaleDateString("en-EN", options);
+};
